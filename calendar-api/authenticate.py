b8321deb65d36de64efe322f99c9bf70cc0379de
paigelee05 <plee65@illinois.edu>
2024-02-25 17:18:06 -0600

    Initialize Google Calendar API and authenticate user.

---
+++ calendar-api/authenticate.py
@@ -0,0 +1,39 @@
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+
+SCOPES = ["https://www.googleapis.com/auth/calendar"]
+
+
+def authenticate():
+  """Authenticates user with Google Calendar API.
+
+  Checks if token.json exists. If it does, the user's access and refesh tokens 
+  are loaded. If there are no (valid) credentials, the user can log in.
+
+  Credentials are stored in token.json after the first time for future use.
+
+  Returns the credentials.
+  """
+  creds = None
+  # The file token.json stores the user's access and refresh tokens, and is
+  # created automatically when the authorization flow completes for the first
+  # time.
+  if os.path.exists("token.json"):
+    creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+  # If there are no (valid) credentials available, let the user log in.
+  if not creds or not creds.valid:
+    if creds and creds.expired and creds.refresh_token:
+      creds.refresh(Request())
+    else:
+      flow = InstalledAppFlow.from_client_secrets_file(
+          "calendar-api/credentials.json", SCOPES
+      )
+      creds = flow.run_local_server(port=0)
+    # Save the credentials for the next run
+    with open("token.json", "w") as token:
+      token.write(creds.to_json())
+
+  return creds
