381701509938198ffec23a573525b1616a1a28d4
bwang086 <bw-25@outlook.com>
2024-02-26 15:37:25 -0600

    Created login & registration pages Followed tutorial to create login and registration pages. Text boxes accept input and added links to toggle between pages. Still need to fix functionality of buttons.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,33 @@
+import React, {useState} from "react"
 import logo from './logo.svg';
 import './App.css';
+import { Login } from "./Login.js";
+import { Register } from "./Register.js";
 
 function App() {
+  const [currentForm, setCurrentForm] = useState('login');
+
+  const toggleForm = (formName) => {
+    setCurrentForm(formName);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        currentForm == "login" ? <Login onFormSwitch = {toggleForm} /> : <Register onFormSwitch = {toggleForm} />
+      }
     </div>
   );
 }
+
+/* function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+@ -40,6 +21,5 @@ function App() {
+    </div>
+  );
+}
+*/
 
 export default App;
